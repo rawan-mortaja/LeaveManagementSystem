63638c1591d605430dd6d7a2373b4eea179da7e9
rawan-mortaja <rawanmortaja077@gmail.com>
2023-08-12 10:54:09 +0300

    Leave Management System

--- app/Http/Controllers/LeaveTypeController.php
+++ app/Http/Controllers/LeaveTypeController.php
@@ -7,59 +7,6 @@ use Illuminate\Http\Request;
 
 class LeaveTypeController extends Controller
 {
-    // public function index()
-    // {
-    //     $leaveTypes = LeaveType::all();
-    //     return view('leave-types.index', compact('leaveTypes'));
-    // }
-
-    // public function create()
-    // {
-    //     return view('leave-types.create');
-    // }
-
-    // public function store(Request $request)
-    // {
-    //     $request->validate([
-    //         'name' => 'required|unique:leave_types,name',
-    //     ]);
-
-    //     LeaveType::create($request->all());
-
-    //     return redirect()->route('leave-types.index')
-    //         ->with('success', 'Leave type created successfully.');
-    // }
-
-    // public function show(LeaveType $leaveType)
-    // {
-    //     return view('leave-types.show', compact('leaveType'));
-    // }
-
-    // public function edit(LeaveType $leaveType)
-    // {
-    //     return view('leave-types.edit', compact('leaveType'));
-    // }
-
-    // public function update(Request $request, LeaveType $leaveType)
-    // {
-    //     $request->validate([
-    //         'name' => 'required|unique:leave_types,name,' . $leaveType->id,
-    //     ]);
-
-    //     $leaveType->update($request->all());
-
-    //     return redirect()->route('leave-types.index')
-    //         ->with('success', 'Leave type updated successfully.');
-    // }
-
-    // public function destroy(LeaveType $leaveType)
-    // {
-    //     $leaveType->delete();
-
-    //     return redirect()->route('leave-types.index')
-    //         ->with('success', 'Leave type deleted successfully.');
-    // }
-
     public function index()
     {
         $leaveTypes = LeaveType::all();
